24cd8fd79baa416e160381aa7f56fc6c01677de8
adomas000 <suvumas000@gmail.com>
2018-01-27 03:44:26 +0200

    Day cycle v000001 is done

---
+++ Assets/Resources/Scripts/sunCycle.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sunCycle : MonoBehaviour {
+    public float sunDistanceFromCamera = 20f;
+    public float sunRotationSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+        transform.eulerAngles = new Vector3(0, 0, 0);   
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(0.05f, 0, 0));
+    }
+
+    
+}
