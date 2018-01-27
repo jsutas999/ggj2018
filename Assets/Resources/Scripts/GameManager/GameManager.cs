12c02d7d55b6c158e18001f95942d9d8de0c7f5c
Justinas Cicenas <jsutas999@gmail.com>
2018-01-27 20:25:54 +0200

    CHN: GameManager speed separation.

--- Assets/Resources/Scripts/GameManager/GameManager.cs
+++ Assets/Resources/Scripts/GameManager/GameManager.cs
@@ -18,7 +18,7 @@ public class GameManager : MonoBehaviour {
 		
 	}
 
-
+    // Depricated
     public void SetSpeed(float speed)
     {
         RoadSegmentManager.SetSpeed(speed);
@@ -26,8 +26,30 @@ public class GameManager : MonoBehaviour {
         CarSegmentManager.SetSpeed(speed);
     }
 
+    // Depricated
     public float GetSpeed()
     {
         return RoadSegmentManager.GetSpeed();
+    }
+
+    public float GetSpeedScenery()
+    {
+        return RoadSegmentManager.GetSpeed();
+    }
+
+    public float GetSpeedCars()
+    {
+        return CarSegmentManager.GetSpeed();
+    }
+
+    public void SetSpeedScenery(float speed)
+    {
+        RoadSegmentManager.SetSpeed(speed);
+        TerrainSegmentManager.SetSpeed(speed);
+    }
+
+    public void SetSpeedCars(float speed)
+    {
+        CarSegmentManager.SetSpeed(speed);
     }
 }
