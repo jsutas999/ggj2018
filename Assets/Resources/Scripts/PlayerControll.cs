c26290c1b2f2d270fc6a15fc86f876854829b066
Tesliukas <lukaskliucinskas@gmail.com>
2018-01-27 17:37:23 +0200

    player control

--- Assets/Resources/Scripts/PlayerControll.cs
+++ Assets/Resources/Scripts/PlayerControll.cs
@@ -7,7 +7,7 @@ public class PlayerControll : MonoBehaviour {
     public float moveSpeed = 1;
     public GameObject car;
     public GameObject playerToss;
-    public float force, height, side;
+    public float height, side;
     public GameObject cam;
     public GameObject jumpPoint;
     public GameManager gm;
@@ -26,7 +26,7 @@ public class PlayerControll : MonoBehaviour {
         // gm.SetSpeed(gm.GetSpeed() + h);
         gm.SetSpeed(15 + v * 5f);
 
-        force = 150 + v * 50;
+        height = 150 + v * 50;
         side = rb.velocity.x * 50;
         rb.AddForce(new Vector3(h * moveSpeed * Time.deltaTime * 100, 0, 0));
         if (Input.GetKeyDown(KeyCode.Space))
@@ -37,16 +37,17 @@ public class PlayerControll : MonoBehaviour {
         Crash();
     }
     void Crash() {
-        if (Physics.Raycast(transform.position, -transform.up, 40))
-            print("There is something down of the object!");
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position + new Vector3(0, 0.175f, 0), -Vector3.up, out hit))
+            Debug.Log(hit);
         GameObject toss;
         toss = Instantiate(playerToss, jumpPoint.transform.position, Quaternion.identity);
         toss.SetActive(true);
+        //rb.AddTorque(new Vector3(5, 0, Random.Range(-1, 1)));
         pToss = toss.GetComponent<PlayerToss>();
-        pToss.force = force;
         pToss.height = height;
         pToss.side = side;
-        car.transform.parent = null;
+        car.transform.parent = hit.transform;
         cam.GetComponent<CameraFollow>().target = toss;
         gameObject.SetActive(false);
 
