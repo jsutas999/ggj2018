6bb917993246910b56bd078aa701d3e902a08e76
adomas000 <suvumas000@gmail.com>
2018-01-28 00:42:00 +0200

    first try on fuel

--- Assets/Resources/Scripts/PlayerControll.cs
+++ Assets/Resources/Scripts/PlayerControll.cs
@@ -13,27 +13,43 @@ public class PlayerControll : MonoBehaviour {
     public GameManager gm;
     public PlayerToss pToss;
 
+    private FuelBar fb;
+    
+
 
 	void Start () {
         rb = GetComponent<Rigidbody>();
         car.transform.parent = transform;
+        fb = transform.GetComponent<FuelBar>();
     }
 
-	void Update () {
+	void FixedUpdate () {
         float v, h;
         h = Input.GetAxis("Horizontal");
         v = Input.GetAxis("Vertical");
 
         // gm.SetSpeed(gm.GetSpeed() + h);
-        gm.SetSpeedScenery(20 + v * 5f);
-        gm.SetSpeedCars(10 + v * 5f);
-
+        if (fb.fuel > 0)
+        {
+            gm.SetSpeedScenery(20 + v * 5f);
+            gm.SetSpeedCars(10 + v * 5f);
+        }else
+        {
+            //something to make car stop
+        }
+        
+        
         height = 200 + v * 100;
         side = rb.velocity.x * 50;
         transform.position += new Vector3(h * Time.deltaTime * moveSpeed, 0, 0);
         //rb.AddForce(new Vector3(h * moveSpeed * Time.deltaTime * 100, 0, 0));
         if (Input.GetKeyDown(KeyCode.Space))
             Crash();
+
+        //fuel
+        fb.fuel -= 0.002f + v/700;
+        
+        Debug.Log(v);
     }
     private void OnCollisionEnter(Collision collision)
     {
